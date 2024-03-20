4578e57c1b8a6a9ceb9db3ef956e2ae053150112
nyabingenorv <norvyphill@gmail.com>
2024-03-20 15:40:34 +0300

    mysql backup ; mysql; rev 2

--- mysql/grant_permision.sql
+++ mysql/grant_permision.sql
@@ -1,4 +1,4 @@
 -- reate a MySQL user named holberton_user on both web-01 and web-02 with the host name set to localhost and the password projectcorrection280hbtn. This will allow us to access the replication status on both servers.
 -- Make sure that holberton_user has permission to check the primary/replica status of your databases.
 -- In addition to that, make sure that task #3 of your SSH project is completed for web-01 and web-02. You will likely need to add the public key to web-02 as you only added it to web-01 for this project. The checker will connect to your servers to check MySQL status
-CREATE USER 'holberton_user'@'localhost';
+CREATE USER 'holberton_user'@'localhost'IDENTIFIED BY 'projectcorrection280hbtn';
